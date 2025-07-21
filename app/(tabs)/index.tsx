7304cbb6f9315e49e3c8daccd151d78730131b66
Guilherme Tessarini <guilherme.tessarini@hotmail.com>
2025-07-21 14:58:34 -0300

    feat: implementa filmes em alta com Appwrite
    - Backend:
      - Integração com Appwrite
      - Contagem de buscas armazenada
    - Frontend:
      - Novo componente `TrendingCard`
      - Exibição nas abas `Index` e `Search`
      - Máscara para conteúdo protegido

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -15,10 +15,18 @@ import { fetchMovies } from "@/services/api";
 import useFetch from "@/services/useFetch";
 
 import MovieCard from "@/components/MovieCard";
+import TrendingCard from "@/components/TrendingCard";
+import { getTrendingMovies } from "@/services/appwrite";
 import { useRouter } from "expo-router";
 
 export default function Index() {
   const router = useRouter();
+
+  const {
+    data: trendingMovies,
+    loading: trendingMoviesLoading,
+    error: trendingMoviesError,
+  } = useFetch(getTrendingMovies);
 
   const {
     data: movies,
@@ -26,7 +34,7 @@ export default function Index() {
     error: moviesError,
   } = useFetch(() =>
     fetchMovies({
-      query: '',
+      query: "",
     })
   );
 
@@ -40,14 +48,16 @@ export default function Index() {
       >
         <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
 
-        {moviesLoading ? (
+        {moviesLoading || trendingMoviesLoading ? (
           <ActivityIndicator
             size="large"
             color="#0000ff"
             className="mt-10 self-center"
           />
-        ) : moviesError ? (
-          <Text>Error: {moviesError?.message}</Text>
+        ) : moviesError || trendingMoviesError ? (
+          <Text>
+            Error: {moviesError?.message || trendingMoviesError?.message}
+          </Text>
         ) : (
           <View className="flex-1 mt-5">
             <SearchBar
@@ -55,18 +65,34 @@ export default function Index() {
               placeholder="Search for a movie"
             />
 
+            {trendingMovies && (
+              <View className="mt-10">
+                <Text className="text-lg text-white font-bold mb-3">
+                  Trending Movies
+                </Text>
+              </View>
+            )}
+
             <>
+              <FlatList
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                ItemSeparatorComponent={() => <View className="w-4" />}
+                className="mb-4 mt-3"
+                data={trendingMovies}
+                renderItem={({ item, index }) => (
+                  <TrendingCard movie={item} index={index} />
+                )}
+                keyExtractor={(item) => item.movie_id.toString()}
+              />
+
               <Text className="text-lg text-white font-bold mt-5 mb-3">
                 Latest Movies
               </Text>
 
               <FlatList
                 data={movies}
-                renderItem={({ item }) => (
-                  <MovieCard 
-                    {...item}
-                  />
-                )}
+                renderItem={({ item }) => <MovieCard {...item} />}
                 keyExtractor={(item) => item.id.toString()}
                 numColumns={3}
                 columnWrapperStyle={{
